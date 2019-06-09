ef9fe12c1eb260168b438dad1c312fa9ee1d4fd3
ngduylinh <49985179+ngduylinh@users.noreply.github.com>
2019-06-09 09:18:20 +0700

    Add files via upload

---
+++ BaiTapLon/javascript/js.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+
+    $('.menu li').hover(
+        function () {
+
+            $('.sub_menu', this).animate({
+                opacity: 1,
+                height: 'toggle'
+            }, 150, function () {
+                // Animation complete.
+            });
+
+        },
+        function () {
+            //hide its submenu
+            $('.sub_menu', this).slideUp(100);
+        }
+    );
+
+});
